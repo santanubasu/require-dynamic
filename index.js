dc31763dcda8c527535f2fb1cec1d3945e23c077
santanubasu <santanu.basu@gmail.com>
2015-11-10 16:20:35 -0700

    Make require hijacking an optional call

--- index.js
+++ index.js
@@ -5,123 +5,123 @@ var _ = require("underscore");
 var tenantCache = {};
 var transforms = {};
 var activeTenant = "";
+var tenantLocalFlag = "_tenantLocal";
 
+module.exports = {
+    tenantLocalFlag:tenantLocalFlag,
+    replaceRequire: _.once(function() {
 // Require hijacking is based on https://github.com/bahmutov/really-need
 
 // these variables are needed inside eval _compile
-/* jshint -W098 */
-var runInNewContext = require('vm').runInNewContext;
-var runInThisContext = require('vm').runInThisContext;
-var path = require('path');
+        /* jshint -W098 */
+        var runInNewContext = require('vm').runInNewContext;
+        var runInThisContext = require('vm').runInThisContext;
+        var path = require('path');
 
-var _require = Module.prototype.require;
-var _compile = Module.prototype._compile;
+        var _require = Module.prototype.require;
+        var _compile = Module.prototype._compile;
 
-function noop() {}
+        function noop() {}
 
-function logger(options) {
-    return check.object(options) &&
-    (options.debug || options.verbose) ? console.log : noop;
-}
-
-function argsToDeclaration(args) {
-    var names = Object.keys(args);
-    return names.map(function (name) {
-            var val = args[name];
-            var value = check.fn(val) ? val.toString() : JSON.stringify(val);
-            return 'var ' + name + ' = ' + value + ';';
-        }).join('\n') + '\n';
-}
-
-function load(transform, module, filename) {
-    var fs = require('fs');
-    var source = fs.readFileSync(filename, 'utf8');
-    var transformed = transform(source, filename);
-    if (check.string(transformed)) {
-        module._compile(transformed, filename);
-    } else {
-        console.error('transforming source from', filename, 'has not returned a string');
-        module._compile(source, filename);
-    }
-}
-
-Module.prototype.require = function(name, tenantArg, options) {
-
-    options = options||{};
-
-    var tenantLocal = false;
-
-    if (_.isString(tenantArg)) {
-        tenantLocal = true;
-        activeTenant = tenantArg;
-    }
-    else if (tenantArg===true) {
-        tenantLocal = true;
-    }
-
-    var nameToLoad;
-    var result;
-
-    try {
-        if (transforms[activeTenant] && name in transforms[activeTenant]) {
-            nameToLoad = Module._resolveFilename(transforms[activeTenant][name], this);
+        function logger(options) {
+            return check.object(options) &&
+            (options.debug || options.verbose) ? console.log : noop;
         }
-        else {
-            nameToLoad = Module._resolveFilename(name, this);
-        }
-    }
-    catch (e) {
-        throw "Unable to resolve dependency "+name+" in dynamic require.";
-    }
 
-    try {
-        if (tenantLocal) {
-            if (nameToLoad in tenantCache[activeTenant]) {
-                result = tenantCache[activeTenant][nameToLoad];
-            }
-            else {
-                result = Module._load(nameToLoad, this);
-                tenantCache[activeTenant][nameToLoad] = result;
-                delete require.cache[nameToLoad];
+        function argsToDeclaration(args) {
+            var names = Object.keys(args);
+            return names.map(function (name) {
+                    var val = args[name];
+                    var value = check.fn(val) ? val.toString() : JSON.stringify(val);
+                    return 'var ' + name + ' = ' + value + ';';
+                }).join('\n') + '\n';
+        }
+
+        function load(transform, module, filename) {
+            var fs = require('fs');
+            var source = fs.readFileSync(filename, 'utf8');
+            var transformed = transform(source, filename);
+            if (check.string(transformed)) {
+                module._compile(transformed, filename);
+            } else {
+                console.error('transforming source from', filename, 'has not returned a string');
+                module._compile(source, filename);
             }
         }
-        else {
-            result = _require.call(this, nameToLoad);
-        }
-    }
-    catch (e) {
-        console.log("Unable to load dependency "+name+" in dynamic require, resolved name was "+nameToLoad);
-        throw e;
-    }
 
-    return result;
-};
+        Module.prototype.require = function(name, tenantArg, options) {
 
-var resolvedArgv;
+            options = options||{};
+
+            var tenantLocal = false;
+
+            if (_.isString(tenantArg)) {
+                tenantLocal = true;
+                activeTenant = tenantArg;
+            }
+            else if (tenantArg===true) {
+                tenantLocal = true;
+            }
+
+            var nameToLoad;
+            var result;
+
+            try {
+                if (transforms[activeTenant] && name in transforms[activeTenant]) {
+                    nameToLoad = Module._resolveFilename(transforms[activeTenant][name], this);
+                }
+                else {
+                    nameToLoad = Module._resolveFilename(name, this);
+                }
+            }
+            catch (e) {
+                throw "Unable to resolve dependency "+name+" in dynamic require.";
+            }
+
+            try {
+                if (tenantCache[activeTenant] && nameToLoad in tenantCache[activeTenant]) {
+                    result = tenantCache[activeTenant][nameToLoad];
+                }
+                else {
+                    result = Module._load(nameToLoad, this);
+                    if (tenantLocal||result[tenantLocalFlag]) {
+                        tenantCache[activeTenant][nameToLoad] = result;
+                        delete require.cache[nameToLoad];
+                    }
+                }
+            }
+            catch (e) {
+                console.log("Unable to load dependency "+name+" in dynamic require, resolved name was "+nameToLoad);
+                throw e;
+            }
+
+            return result;
+        };
+
+        var resolvedArgv;
 
 // see Module.prototype._compile in
 // https://github.com/joyent/node/blob/master/lib/module.js
-var _compileStr = _compile.toString();
-_compileStr = _compileStr.replace('self.require(path);', 'self.require.apply(self, arguments);');
+        var _compileStr = _compile.toString();
+        _compileStr = _compileStr.replace('self.require(path);', 'self.require.apply(self, arguments);');
 
-/* jshint -W061 */
-var patchedCompile = eval('(' + _compileStr + ')');
+        /* jshint -W061 */
+        var patchedCompile = eval('(' + _compileStr + ')');
 
-Module.prototype._compile = function (content, filename) {
-    var result = patchedCompile.call(this, content, filename);
-    return result;
-};
+        Module.prototype._compile = function (content, filename) {
+            var result = patchedCompile.call(this, content, filename);
+            return result;
+        };
 
-var dynamicRequire = Module.prototype.require.bind(module.parent);
-dynamicRequire.cache = require.cache;
-
-module.exports = {
-    require:dynamicRequire,
-    register:function(key, transform) {
-        tenantCache[key] = {};
-        transforms[key] = transform;
+        var dynamicRequire = Module.prototype.require.bind(module.parent);
+        dynamicRequire.cache = require.cache;
+        return dynamicRequire;
+    }),
+    register:function(tenant, transform) {
+        tenantCache[tenant] = {};
+        transforms[tenant] = transform;
     },
-    activate:function(key) {
-        activeTenant = key;
+    activate:function(tenant) {
+        activeTenant = tenant;
     }
 }
